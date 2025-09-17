3634e3ca8234f08f7b3ef9ba2629fcd2712a570f
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-09-17 08:33:49 -0300

    first commit

---
+++ src/test/java/br/com/fiap/BackBank/BackBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.BackBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
