8798fbae7c0224e3139137258260f1a41db5d5a0
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-09-24 10:43:55 -0300

    criando o controller de projeto

---
+++ src/main/java/br/com/fiap/BackBank/controller/ProjectController.java
@@ -0,0 +1,25 @@
+package br.com.fiap.BackBank.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ProjectController {
+    private final Map<String, List<String>> project = new HashMap<>();
+
+    @GetMapping
+    public Map<String,List<String>> getProjectMap() {
+        project.put("nameProject", List.of("BackBank"));
+        project.put("students", List.of("João Vinicius Alves", "Matheus Mariotto"));
+        project.put("rm", List.of("rm559369", "rm560376"));
+
+        return project;
+    }
+    
+}
